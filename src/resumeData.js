0c19e804d2cfb74b668397bf628902d65ec6df75
Mihir Srivastava <srimihirprakhar@gmail.com>
2021-09-06 23:04:31 +0530

    commit

---
+++ src/resumeData.js
@@ -0,0 +1,65 @@
+let resumeData = {
+    "imagebaseurl":"https://avatars.githubusercontent.com/u/53559024?s=60&v=4",
+    "name": "Mihir Srivastava",
+    "role": "Engineering Undergraduate",
+    "linkedinId":"https://www.linkedin.com/in/mihir-srivastava-937387191/",
+    "roleDescription": "I like to try new technologies and frameworks, in my free time I listen to music",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/mihir-srivastava-937387191/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/MihirSrivastava-tech",
+          "className":"fa fa-github"
+        },
+
+      ],
+    "aboutme":"I am currently pursuing B.tech with Electrical Engineering as my major. I am a front end and VR developer. I know a little bit of backend technologies also.",
+    "address":"India",
+    "website":" ",
+    "education":[
+      {
+        "UniversityName":"MMMUT",
+        "specialization":"Electrical Engineering",
+        "YearOfPassing":"2019-2023",
+      }
+
+    ],
+    "skillsDescription":"These are some of my skills.",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"aframe"
+      },
+      {
+        "skillname":"AR-js"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"V-One",
+        "description":"VR world of monuments.",
+        "imgurl":"images/VR.jpg"
+      },
+      {
+        "name":"SpaceAR",
+        "description":"Web AR works on marker for placing 3D objects in real world.",
+        "imgurl":"images/AR.jpg"
+      },
+
+    ]
+    
+  }
+  
+  export default resumeData
