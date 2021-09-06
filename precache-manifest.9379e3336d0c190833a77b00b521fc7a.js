cc52c406b12db1786aacf0ea64c7ff6ea80f13cf
Mihir Srivastava <srimihirprakhar@gmail.com>
2021-09-06 22:36:47 +0530

    Updates

---
+++ precache-manifest.9379e3336d0c190833a77b00b521fc7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4ad3bc3fe865d662087",
+    "url": "/ReactPortfolio/static/css/main.6c856aff.chunk.css"
+  },
+  {
+    "revision": "a4ad3bc3fe865d662087",
+    "url": "/ReactPortfolio/static/js/main.a4ad3bc3.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/ReactPortfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "aa22ff2d541828390b91",
+    "url": "/ReactPortfolio/static/js/runtime~main.aa22ff2d.js"
+  },
+  {
+    "revision": "e95ac128b3c675990cb9e241c60e1140",
+    "url": "/ReactPortfolio/index.html"
+  }
+];
